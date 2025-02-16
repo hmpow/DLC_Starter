bea67cacb23198a79ad21304f74e546a4090d51f
hmpow <67511807+hmpow@users.noreply.github.com>
2025-02-16 19:11:04 +0900

    RTC動作

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,22 @@
 
 #include <Arduino.h>
 #include <WiFiS3.h>
+#include <RTC.h>
+
 #include "arduino_secrets.h"
 #include "web_page.h"
 
 // #include <WiFiServer.h> //WiFiS3.h → WiFi.h → WiFiServer.h でインクルードされる
-
-// https://docs.arduino.cc/language-reference/en/functions/wifi/server/
-// https://docs.arduino.cc/language-reference/en/functions/wifi/client/
-// https://docs.arduino.cc/libraries/wifi/
-
 
 /* プロトタイプ宣言 PlatformIO では必要 */
 void blinkLED(int);
 void sendHTML(WiFiClient, const char*);
 void send404(WiFiClient);
 void printWiFiStatus(void);
+
+
+void printRTCtime(void);
+void setupRTC(void);
 
 
 /* 設定 */
@@ -26,14 +27,19 @@ const char* password = SECRET_PASS;  // パスワード
 
 WiFiServer server(80);  // Webサーバーのポート
 
+
 const int ledPin = LED_BUILTIN;  // 内蔵LEDのピン
 int blinkCount = 0;  // LEDの点滅回数
 
 /* メイン関数 */
 void setup() {
+  delay(10000); //Platform IO がアップロードタスクからシリアルモニタタスクに戻るのを待つ
+
   Serial.begin(9600);
 
-  // check for the WiFi module:
+  setupRTC();
+
+  // WiFi モジュール搭載の確認:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
     // don't continue
@@ -42,24 +48,19 @@ void setup() {
   
   // アクセスポイントの開始
   WiFi.beginAP(ssid, password);
-  delay(500);
   while (WiFi.status() != WL_AP_LISTENING) {
     delay(500);
     Serial.print(".");
   }
-  delay(500);
   Serial.println("\nアクセスポイント開始: " + String(ssid));
-  delay(500);
+
   // サーバー開始
   server.begin();
   Serial.println("Webサーバー開始");
-  delay(500);
   pinMode(ledPin, OUTPUT);
 
   delay(1000);
-
   printWiFiStatus();
-  delay(500);
 }
 
 void loop() {
@@ -80,6 +81,7 @@ void loop() {
 
     if (request.indexOf("GET / ") != -1) {
       sendHTML(client, HTML_HOME);  // ホームページ
+      printRTCtime();
     }
     else if (request.indexOf("GET /led") != -1) {
       if (request.indexOf("count=") != -1) {
@@ -141,4 +143,53 @@ void printWiFiStatus() {
   Serial.print("To see this page in action, open a browser to http://");
   Serial.println(ip);
 
+}
+
+
+void printRTCtime(void){
+  RTCTime currentTime;
+  bool running = RTC.isRunning();
+
+  if (running) {
+    Serial.println("RTC is running!");
+  }else{
+    Serial.println("RTC is not running!");
+  }
+
+  // Get current time from RTC
+  RTC.getTime(currentTime);
+
+  // Print out UNIX time
+  Serial.print("UNIX time: ");
+  Serial.println(currentTime.getUnixTime());
+
+
+  Serial.print("YYYY/MM/DD - HH/MM/SS:");
+  Serial.print(currentTime.getYear());
+  Serial.print("/");
+  Serial.print(Month2int(currentTime.getMonth()));
+  Serial.print("/");
+  Serial.print(currentTime.getDayOfMonth());
+  Serial.print(" - ");
+  Serial.print(currentTime.getHour());
+  Serial.print(":");
+  Serial.print(currentTime.getMinutes());
+  Serial.print(":");
+  Serial.println(currentTime.getSeconds());
+}
+
+
+void setupRTC(void){
+  //何故かRTCの開始と時刻設定が一体化しているAPI仕様のため
+  //一旦取得して時刻設定という動きをしないと枚リセットごとに時計が初期化されてしまう
+
+  RTC.begin(); // RTCの初期化　これだけではRTC動き始めない
+  printRTCtime(); //表示
+
+  RTCTime rtcTime;
+  RTC.getTime(rtcTime);// RTCから現在時刻を取得
+
+  RTC.setTimeIfNotRunning(rtcTime); // 現在時刻を引き継いでRTCをスタート
+
+  printRTCtime(); //表示
 }
