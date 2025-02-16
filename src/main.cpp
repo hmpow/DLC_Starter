480a5cd855d6203e1622c5d54eaf788e38448097
hmpow <67511807+hmpow@users.noreply.github.com>
2025-02-16 15:31:24 +0900

    設定のページ分割

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,17 @@
 
 // #include <WiFiServer.h> //WiFiS3.h → WiFi.h → WiFiServer.h でインクルードされる
 
+// https://docs.arduino.cc/language-reference/en/functions/wifi/server/
+// https://docs.arduino.cc/language-reference/en/functions/wifi/client/
+// https://docs.arduino.cc/libraries/wifi/
+
 
 /* プロトタイプ宣言 PlatformIO では必要 */
 void blinkLED(int);
-void sendHTML(WiFiClient);
+void sendHTML(WiFiClient, const char*);
+void send404(WiFiClient);
 void printWiFiStatus(void);
+
 
 /* 設定 */
 
@@ -26,24 +32,34 @@ int blinkCount = 0;  // LEDの点滅回数
 /* メイン関数 */
 void setup() {
   Serial.begin(9600);
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }  
   
   // アクセスポイントの開始
   WiFi.beginAP(ssid, password);
+  delay(500);
   while (WiFi.status() != WL_AP_LISTENING) {
     delay(500);
     Serial.print(".");
   }
+  delay(500);
   Serial.println("\nアクセスポイント開始: " + String(ssid));
-  
+  delay(500);
   // サーバー開始
   server.begin();
   Serial.println("Webサーバー開始");
-  
+  delay(500);
   pinMode(ledPin, OUTPUT);
 
-  delay(10000);
+  delay(1000);
 
   printWiFiStatus();
+  delay(500);
 }
 
 void loop() {
@@ -61,17 +77,25 @@ void loop() {
       }
     }
 
-    // クエリパラメータから点滅回数を取得
-    if (request.indexOf("GET /?count=") != -1) {
-      int start = request.indexOf("count=") + 6;
-      int end = request.indexOf(" ", start);
-      blinkCount = request.substring(start, end).toInt();
-      Serial.println("設定された点滅回数: " + String(blinkCount));
-      blinkLED(blinkCount);
-    }
 
-    // HTMLレスポンスを送信
-    sendHTML(client);
+    if (request.indexOf("GET / ") != -1) {
+      sendHTML(client, HTML_HOME);  // ホームページ
+    }
+    else if (request.indexOf("GET /led") != -1) {
+      if (request.indexOf("count=") != -1) {
+        int start = request.indexOf("count=") + 6;
+        int end = request.indexOf(" ", start);
+        blinkCount = request.substring(start, end).toInt();
+        Serial.println("設定された点滅回数: " + String(blinkCount));
+        blinkLED(blinkCount);
+      }
+      sendHTML(client, HTML_LED);  // LED設定ページ
+    }
+    else {
+      //send404(client);  // 存在しないページ
+      sendHTML(client, HTML_HOME);  // ホームページを表示
+    }
+    
     client.stop();
   }
 }
@@ -87,12 +111,20 @@ void blinkLED(int count) {
 }
 
 // HTMLページを送信する関数
-void sendHTML(WiFiClient client) {
+void sendHTML(WiFiClient client, const char* page) {
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
   client.println("Connection: close");
   client.println();
-  client.println(SETTING_HTML);
+  client.println(page);
+}
+
+void send404(WiFiClient client) {
+  client.println("HTTP/1.1 404 Not Found");
+  client.println("Content-Type: text/html");
+  client.println("Connection: close");
+  client.println();
+  client.println("<h1>404 Not Found</h1>");
 }
 
 void printWiFiStatus() {
--- src/web_page.h
+++ src/web_page.h
@@ -3,24 +3,45 @@
 
 #include <Arduino.h>
 
-const String SETTING_HTML = R"rawliteral(
-<!DOCTYPE html>
-<html>
-<meta lang='ja'>
-<head>
-  <meta charset='utf-8'>
-  <meta name='viewport' content='width=device-width, initial-scale=1'>
-  <title>LED点滅設定</title>
-</head>
-<body>
-  <h1>LED点滅設定 外部ファイルから</h1>
-  <form action='/' method='get'>
-    点滅回数: <input type='number' name='count' min='1' max='20'>
-    <br>
-    <input type='submit' value='設定'>
-  </form>
-</body>
-</html>
-)rawliteral";
+// ホームページ
+const char HTML_HOME[] PROGMEM = R"rawliteral(
+    <!DOCTYPE html>
+    <html>
+    <meta lang='ja'>
+    <head>
+      <meta charset='utf-8'>
+      <meta name='viewport' content='width=device-width, initial-scale=1'>
+      <title>ホーム</title>
+    </head>
+    <body>
+      <h1>設定メニュー</h1>
+      <ul>
+        <li><a href='/led'>LEDの設定</a></li>
+      </ul>
+    </body>
+    </html>
+    )rawliteral";
+    
+    // LED設定ページ
+    const char HTML_LED[] PROGMEM = R"rawliteral(
+    <!DOCTYPE html>
+    <html>
+    <meta lang='ja'>
+    <head>
+      <meta charset='utf-8'>
+      <meta name='viewport' content='width=device-width, initial-scale=1'>
+      <title>LED設定</title>
+    </head>
+    <body>
+      <h1>LED点滅設定</h1>
+      <form action='/led' method='get'>
+        点滅回数: <input type='number' name='count' min='1' max='20'>
+        <input type='submit' value='設定'>
+      </form>
+      <br>
+      <a href='/'>← ホームへ戻る</a>
+    </body>
+    </html>
+    )rawliteral";
 
 #endif
